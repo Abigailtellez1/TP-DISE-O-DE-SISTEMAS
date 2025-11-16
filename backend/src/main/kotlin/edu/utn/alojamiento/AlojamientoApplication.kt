a0678d7c6cc5b556ce39944924abaf4287c0368c
Ignacio Speicys <ignaciospeicys@gmail.com>
2025-11-15 22:40:19 -0300

    base de backend usando https://start.spring.io/

---
+++ backend/src/main/kotlin/edu/utn/alojamiento/AlojamientoApplication.kt
@@ -0,0 +1,11 @@
+package edu.utn.alojamiento
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlojamientoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlojamientoApplication>(*args)
+}
